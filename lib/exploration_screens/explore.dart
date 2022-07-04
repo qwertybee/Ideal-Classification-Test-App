7e0e795e69b58c30d996477cdcb6fae258d6d25c
Rinradee Sae-lao <rinradee.sae@gmail.com>
2022-07-05 01:22:22 +0700

    Changed exploration page structure

--- lib/exploration_screens/explore.dart
+++ lib/exploration_screens/explore.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project_2/exploration_screens/body.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class Explore extends StatefulWidget {
   const Explore({Key? key}) : super(key: key);
@@ -12,13 +12,68 @@ class _ExploreState extends State<Explore> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false, // might be problem in future
-        elevation: 0,
-        backgroundColor: const Color(0xffce93d8),
-        toolbarHeight: 100,
+      body: Padding(
+        padding: EdgeInsets.only(left: 20, top: 50, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [],
+            ),
+            SizedBox(height: 30),
+            Text("Hey dipshit"),
+            Text("Discover jobs you want to know more"),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 30),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+              height: 60,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xFFF5F5F7),
+                borderRadius: BorderRadius.circular(40),
+              ),
+              child: Row(
+                children: [
+                  Text("Search"),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Category"),
+              ],
+            ),
+            SizedBox(height: 30),
+            Expanded(
+                child: StaggeredGridView.countBuilder(
+                    crossAxisCount: 2,
+                    itemCount: 5,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        padding: EdgeInsets.all(20),
+                        height: index.isEven ? 200 : 240,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          color: Colors.purple,
+                          // PUT IMAGES HERE
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // TEXT DATA HERE 5:30
+                          ],
+                        ),
+                      );
+                    }, 
+                    staggeredTileBuilder: (index) => StaggeredTile.fit(1))
+            )
+          ],
+        ),
       ),
-      body: Body(),
     );
   }
 }
