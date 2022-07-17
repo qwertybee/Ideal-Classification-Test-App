4e686f218be66de2717971180279dc3d59176cdd
SwordMaster <43595245+SwordMaster191@users.noreply.github.com>
2022-07-18 01:24:24 +0700

    Fix account registration padding

--- lib/login_screens/login.dart
+++ lib/login_screens/login.dart
@@ -1,8 +1,7 @@
- import 'package:firebase_auth/firebase_auth.dart';
- import 'package:firebase_core/firebase_core.dart';
- import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:project_2/login_screens/register.dart';
-import 'package:project_2/profile_screens/profilepage.dart';
+import 'package:project_2/login_screens/view_local_history.dart';
 
  class LoginScreen extends StatefulWidget {
    const LoginScreen({Key? key}) : super(key: key);
@@ -12,6 +11,7 @@ import 'package:project_2/profile_screens/profilepage.dart';
  class _LoginScreenState extends State<LoginScreen>{
    final emailController = TextEditingController();
    final passwordController = TextEditingController();
+   final _formKey = GlobalKey<FormState>();
 
    @override
    void initState() {
@@ -49,52 +49,70 @@ import 'package:project_2/profile_screens/profilepage.dart';
          Container(
          child: Padding(
          padding: EdgeInsets.all(8.0),
-           child: Column(
-           crossAxisAlignment: CrossAxisAlignment.start,
-           children: <Widget>[
-             Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: Text(
-                   "E-mail address"
-               ),
-             ),
-             TextField(
-               controller: emailController,
-               cursorColor: Colors.black,
-               keyboardType: TextInputType.emailAddress,
-               textInputAction: TextInputAction.next,
-               decoration: const InputDecoration(
-                 border: OutlineInputBorder(),
-                 hintText: 'E-mail address',),
-             ),
-             Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: Text(
-                   "Password"
-               ),
-             ),
-             TextField(
-               controller: passwordController,
-               cursorColor: Colors.black,
-               textInputAction: TextInputAction.done,
-               keyboardType: TextInputType.visiblePassword,
-               obscureText: !_passwordVisible,
-               decoration: InputDecoration(
-                 border: OutlineInputBorder(),
-                 hintText: 'Password',
-                 suffixIcon: IconButton(
-                   icon: Icon(_passwordVisible
-                       ? Icons.visibility
-                       : Icons.visibility_off),
-                   onPressed: () {
-                     setState(() {
-                       _passwordVisible = !_passwordVisible;
-                     });
-                   },
-                 ),
-               ),
-             ),
-            ])
+           child: Form(
+               key: _formKey,
+           child: SingleChildScrollView(
+             child: Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+                 children: <Widget>[
+                   Padding(
+                     padding: const EdgeInsets.all(8.0),
+                     child: Text(
+                         "E-mail address"
+                     ),
+                   ),
+                   TextFormField(
+                     controller: emailController,
+                     cursorColor: Colors.black,
+                     keyboardType: TextInputType.emailAddress,
+                     textInputAction: TextInputAction.next,
+                     decoration: const InputDecoration(
+                       border: OutlineInputBorder(),
+                       hintText: 'E-mail address',),
+                     validator: (value) {
+                       if (value == null || value.isEmpty) {
+                         return 'Please enter your email';
+                       }
+                       return null;
+                     },
+                   ),
+                   Padding(
+                     padding: const EdgeInsets.all(8.0),
+                     child: Text(
+                         "Password"
+                     ),
+                   ),
+                   TextFormField(
+                     controller: passwordController,
+                     cursorColor: Colors.black,
+                     textInputAction: TextInputAction.done,
+                     keyboardType: TextInputType.visiblePassword,
+                     obscureText: !_passwordVisible,
+                     decoration: InputDecoration(
+                       border: OutlineInputBorder(),
+                       hintText: 'Password',
+                       suffixIcon: IconButton(
+                         icon: Icon(_passwordVisible
+                             ? Icons.visibility
+                             : Icons.visibility_off),
+                         onPressed: () {
+                           setState(() {
+                             _passwordVisible = !_passwordVisible;
+                           });
+                         },
+                       ),
+                     ),
+                     validator: (value) {
+                       if (value == null || value.isEmpty) {
+                         return 'Please enter your password';
+                       }
+                       return null;
+                     },
+                   ),
+                 ]
+             )
+           ),
+           )
            ),
           ),
          SizedBox(height: 20),
@@ -114,37 +132,48 @@ import 'package:project_2/profile_screens/profilepage.dart';
              ),
              onPressed: () async {
                FocusManager.instance.primaryFocus?.unfocus();
-               try {
-                 final user = await FirebaseAuth.instance.signInWithEmailAndPassword(
-                     email: emailController.text.trim(), password: passwordController.text.trim());
-                 ScaffoldMessenger.of(context).showSnackBar(
-                   const SnackBar(content: Text('Signed in successfully!')),);
-                 //Navigator.push(context, MaterialPageRoute(builder: (context) => Display(user: user)));
-               } on FirebaseAuthException catch (e) {
-                 print(e);
+                 if (_formKey.currentState!.validate()) {
+                   _formKey.currentState?.save();
+                   try {
+                     final user = await FirebaseAuth.instance
+                         .signInWithEmailAndPassword(
+                         email: emailController.text.trim(),
+                         password: passwordController.text.trim());
+                     ScaffoldMessenger.of(context).showSnackBar(
+                       const SnackBar(
+                           content: Text('Signed in successfully!')),);
+                     //Navigator.push(context, MaterialPageRoute(builder: (context) => Display(user: user)));
+                   } on FirebaseAuthException catch (e) {
+                     print(e);
+                     if (e.code == 'invalid-email') {
+                       ScaffoldMessenger.of(context).showSnackBar(
+                         const SnackBar(content: Text(
+                             'Invalid email specified, please try again.')),);
+                     }
+                     else if (e.code == 'user-not-found') {
+                       ScaffoldMessenger.of(context).showSnackBar(
+                         const SnackBar(content: Text(
+                             'The provided email does not exist in the system, please try again or register.')),);
+                     }
+                     else if (e.code == 'wrong-password') {
+                       ScaffoldMessenger.of(context).showSnackBar(
+                         const SnackBar(content: Text(
+                             'Incorrect password specified, please try again.')),);
+                     }
+                     else if (e.code == 'user-disabled') {
+                       ScaffoldMessenger.of(context).showSnackBar(
+                         const SnackBar(content: Text(
+                             'Account disabled, please contact the app developer.')),);
+                     }
+                     else {
+                       ScaffoldMessenger.of(context).showSnackBar(
+                         const SnackBar(content: Text(
+                             'An error occurred, please try again.')),);
+                     }
+                   }
+                 }
+               },
 
-               if(e.code == 'invalid-email'){
-                   ScaffoldMessenger.of(context).showSnackBar(
-                     const SnackBar(content: Text('Invalid email specified, please try again.')),);
-               }
-               else if(e.code == 'user-not-found') {
-                 ScaffoldMessenger.of(context).showSnackBar(
-                   const SnackBar(content: Text('The provided email does not exist in the system, please try again or register.')),);
-               }
-               else if(e.code == 'wrong-password') {
-                   ScaffoldMessenger.of(context).showSnackBar(
-                     const SnackBar(content: Text('Incorrect password specified, please try again.')),);
-                 }
-               else if(e.code == 'user-disabled') {
-                   ScaffoldMessenger.of(context).showSnackBar(
-                     const SnackBar(content: Text('Account disabled, please contact the app developer.')),);
-                 }
-               else{
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('An error occurred, please try again.')),);
-                 }
-               }
-             },
              child: const Text("Sign In"),
            ),
          ),
@@ -170,6 +199,30 @@ import 'package:project_2/profile_screens/profilepage.dart';
                  Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterScreen()));
                },
              child: const Text("Register"),
+           ),
+         ),
+
+         SizedBox(height: 20),
+         SizedBox(
+           width: 300,
+           height: 40,
+           child: TextButton(
+
+             style: ButtonStyle(
+                 foregroundColor: MaterialStateProperty.all(Colors.white),
+
+                 backgroundColor: MaterialStateProperty.all(Color(0xFFCC41FF)),
+                 shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                     RoundedRectangleBorder(
+                         borderRadius: BorderRadius.circular(12.0),
+                         side: const BorderSide(color: Color(0xFFCC41FF))
+                     )
+                 )
+             ),
+             onPressed: () async {
+               Navigator.push(context, MaterialPageRoute(builder: (context) => ViewLocalHistory()));
+             },
+             child: const Text("View Test History (Local)"),
            ),
          ),
        ],
--- lib/login_screens/register.dart
+++ lib/login_screens/register.dart
@@ -5,16 +5,16 @@ import 'package:project_2/profile_screens/profilecontroller.dart';
 import 'package:project_2/profile_screens/profilepage.dart';
 
  class RegisterScreen extends StatefulWidget {
-   @override
+   const RegisterScreen({Key? key}) : super(key: key);
    State<RegisterScreen> createState() => _RegisterScreenState();
  }
 
  class _RegisterScreenState extends State<RegisterScreen> {
    late FirebaseAuth _auth;
    bool _passwordVisible = false;
-   String name = '';
-   String email = '';
-   String password = '';
+   final nameController = TextEditingController();
+   final emailController = TextEditingController();
+   final passwordController = TextEditingController();
    final _formKey = GlobalKey<FormState>();
 
    @override
@@ -44,182 +44,173 @@ import 'package:project_2/profile_screens/profilepage.dart';
    @override
    Widget build(BuildContext context) {
      return Scaffold(
-       body: Column(
-         children: [
-           SizedBox(height: MediaQuery.of(context).size.height/7),
-           Text("Account Registration",
-             style: TextStyle(
-               fontSize: 28, color: Color(0xFF0D1333), fontWeight: FontWeight.bold,
+       body: Container(
+         padding: EdgeInsets.all(20.0),
+         child: Column(
+           children: [
+             SizedBox(height: MediaQuery.of(context).size.height/7),
+             Text("Account Registration",
+               style: TextStyle(
+                 fontSize: 28, color: Color(0xFF0D1333), fontWeight: FontWeight.bold,
+               ),
              ),
-           ),
-           SizedBox(height: 2),
-           Text("Register now and enjoy benefits from our app!",
-             textAlign: TextAlign.center,
-             style: TextStyle(
-               fontSize: 20, color: Color(0xFF61688B),
+             SizedBox(height: 2),
+             Text("Register now and enjoy benefits from our app!",
+               textAlign: TextAlign.center,
+               style: TextStyle(
+                 fontSize: 20, color: Color(0xFF61688B),
+               ),
              ),
-           ),
-           Container(
-             child: Padding(
-               padding: EdgeInsets.all(15.0),
-               child: Form(
-                 key: _formKey,
-                 child: SingleChildScrollView(
-                   child: Column(
-                     crossAxisAlignment: CrossAxisAlignment.start,
-                     children: <Widget>[
-                       Padding(
-                         padding: const EdgeInsets.all(8.0),
-                         child: Text(
-                             "Name"
+             Container(
+               child: Padding(
+                 padding: EdgeInsets.all(15.0),
+                 child: Form(
+                   key: _formKey,
+                   child: SingleChildScrollView(
+                     child: Column(
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                       children: <Widget>[
+                         Padding(
+                           padding: const EdgeInsets.all(8.0),
+                           child: Text(
+                               "Name"
+                           ),
                          ),
-                       ),
-                       TextFormField(
-                         onSaved: (String? value){
-                           if (value != null){
-                             name = value;
-                           }
-                         },
-                         keyboardType: TextInputType.emailAddress,
-                         decoration: const InputDecoration(
+                         TextFormField(
+                           controller: nameController,
+                           keyboardType: TextInputType.emailAddress,
+                           decoration: const InputDecoration(
                              border: OutlineInputBorder(),
                              hintText: 'Name',),
-                         validator: (value) {
-                           if (value == null || value.isEmpty) {
-                             return 'Please enter your name';
-                           }
-                           return null;
-                         },
-                       ),
-                       Padding(
-                         padding: const EdgeInsets.all(8.0),
-                         child: Text(
-                             "E-mail address"
+                           validator: (value) {
+                             if (value == null || value.isEmpty) {
+                               return 'Please enter your name';
+                             }
+                             return null;
+                           },
                          ),
-                       ),
-                       TextFormField(
-                         onSaved: (String? value){
-                           if (value != null){
-                             email = value;
-                           }
-                         },
-                         keyboardType: TextInputType.emailAddress,
-                         decoration: const InputDecoration(
+                         Padding(
+                           padding: const EdgeInsets.all(8.0),
+                           child: Text(
+                               "E-mail address"
+                           ),
+                         ),
+                         TextFormField(
+                           controller: emailController,
+                           keyboardType: TextInputType.emailAddress,
+                           decoration: const InputDecoration(
                              border: OutlineInputBorder(),
                              hintText: 'E-mail address',),
-                         validator: (value) => validateEmail(value),
-                       ),
-                       Padding(
-                         padding: const EdgeInsets.all(8.0),
-                         child: Text(
-                             "Password"
+                           validator: (value) => validateEmail(value),
                          ),
-                       ),
-                       TextFormField(
-                         onSaved: (String? value){
-                           if (value != null){
-                             password = value;
-                           }
-                         },
-                         obscureText: !_passwordVisible,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: 'Password',
-                            suffixIcon: IconButton(
-                              icon: Icon(_passwordVisible
-                                  ? Icons.visibility
-                                  : Icons.visibility_off),
-                              onPressed: () {
-                                setState(() {
-                                  _passwordVisible = !_passwordVisible;
-                                });
-                              },
-                            ),
-                          ),
-                         keyboardType: TextInputType.visiblePassword,
+                         Padding(
+                           padding: const EdgeInsets.all(8.0),
+                           child: Text(
+                               "Password"
+                           ),
+                         ),
+                         TextFormField(
+                           controller: passwordController,
+                           obscureText: !_passwordVisible,
+                           decoration: InputDecoration(
+                             border: OutlineInputBorder(),
+                             hintText: 'Password',
+                             suffixIcon: IconButton(
+                               icon: Icon(_passwordVisible
+                                   ? Icons.visibility
+                                   : Icons.visibility_off),
+                               onPressed: () {
+                                 setState(() {
+                                   _passwordVisible = !_passwordVisible;
+                                 });
+                               },
+                             ),
+                           ),
+                           keyboardType: TextInputType.visiblePassword,
 
-                         validator: (value) {
-                           if (value == null || value.isEmpty) {
-                             return 'Your password cannot be empty';
-                           }
-                           else if(value.length < 6){
-                             return 'Your password must be at least 6 characters';
-                           }
-                           return null;
-                         },
-                       ),
-                     ],
+                           validator: (value) {
+                             if (value == null || value.isEmpty) {
+                               return 'Your password cannot be empty';
+                             }
+                             else if(value.length < 6){
+                               return 'Your password must be at least 6 characters';
+                             }
+                             return null;
+                           },
+                         ),
+                       ],
+                     ),
                    ),
                  ),
                ),
              ),
-           ),
-           SizedBox(height: 20),
-           SizedBox(
-             width: 300,
-             height: 40,
-             child: TextButton(
-               style: ButtonStyle(
-                   foregroundColor: MaterialStateProperty.all(Colors.white),
-                   backgroundColor: MaterialStateProperty.all(Colors.purple),
-                   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                       RoundedRectangleBorder(
-                           borderRadius: BorderRadius.circular(12.0),
-                           side: const BorderSide(color: Colors.purple)
+             SizedBox(height: 20),
+             SizedBox(
+               width: 300,
+               height: 40,
+               child: TextButton(
+                   style: ButtonStyle(
+                       foregroundColor: MaterialStateProperty.all(Colors.white),
+                       backgroundColor: MaterialStateProperty.all(Colors.purple),
+                       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                           RoundedRectangleBorder(
+                               borderRadius: BorderRadius.circular(12.0),
+                               side: const BorderSide(color: Colors.purple)
+                           )
                        )
-                   )
-               ),
-               child: const Text("Create Account"),
-                 onPressed: () async {
-                   FocusManager.instance.primaryFocus?.unfocus();
-                    if (_formKey.currentState!.validate()) {
-                      _formKey.currentState?.save();
-                      try {
+                   ),
+                   child: const Text("Create Account"),
+                   onPressed: () async {
+                     FocusManager.instance.primaryFocus?.unfocus();
+                     if (_formKey.currentState!.validate()) {
+                       _formKey.currentState?.save();
+                       try {
                          final newUser = await _auth.createUserWithEmailAndPassword(
-                         email: email, password: password);
+                             email: emailController.text, password: passwordController.text);
                          User? user = newUser.user;
-                         user?.updateDisplayName(name);
+                         user?.updateDisplayName(nameController.text);
 
                          ScaffoldMessenger.of(context).showSnackBar(
-                         const SnackBar(content: Text('Registration successful!')),);
+                           const SnackBar(content: Text('Registration successful!')),);
 
                          // Temporary fix so that you can still see the nav screen on the profile page
                          Navigator.pop(context);
                          //Navigator.push(
-                            //context, MaterialPageRoute(builder: (context) => Profile()));
-                        } catch (e) {
-                          print(e);
-                          ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Registration failed.')),);
-                        }
-                      }
+                         //context, MaterialPageRoute(builder: (context) => Profile()));
+                       } catch (e) {
+                         print(e);
+                         ScaffoldMessenger.of(context).showSnackBar(
+                           const SnackBar(content: Text('Registration failed.')),);
+                       }
+                     }
                    }
-             ),
-           ),
-
-           SizedBox(height: 20),
-           SizedBox(
-             width: 300,
-             height: 40,
-             child: TextButton(
-               style: ButtonStyle(
-                   foregroundColor: MaterialStateProperty.all(Colors.white),
-
-                   backgroundColor: MaterialStateProperty.all(Color(0xFFFF6F61)),
-                   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                       RoundedRectangleBorder(
-                           borderRadius: BorderRadius.circular(12.0),
-                           side: const BorderSide(color: Color(0xFFFD6E61))
-                       )
-                   )
                ),
-               onPressed: () {
-                 Navigator.pop(context);
-               },
-               child: const Text("Return to Login"),
              ),
-           ),
-         ],
+
+             SizedBox(height: 20),
+             SizedBox(
+               width: 300,
+               height: 40,
+               child: TextButton(
+                 style: ButtonStyle(
+                     foregroundColor: MaterialStateProperty.all(Colors.white),
+
+                     backgroundColor: MaterialStateProperty.all(Color(0xFFFF6F61)),
+                     shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                         RoundedRectangleBorder(
+                             borderRadius: BorderRadius.circular(12.0),
+                             side: const BorderSide(color: Color(0xFFFD6E61))
+                         )
+                     )
+                 ),
+                 onPressed: () {
+                   Navigator.pop(context);
+                 },
+                 child: const Text("Return to Login"),
+               ),
+             ),
+           ],
+         ),
        ),
      );
    }
